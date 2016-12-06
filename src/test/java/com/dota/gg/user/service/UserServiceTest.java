39f5600289d2d4e05393552e36545aea48696916
dennis <waywaitway@163.com>
2016-12-06 13:36:01 +0800

    ch4 mybatis finished

--- src/test/java/com/dota/gg/user/service/UserServiceTest.java
+++ src/test/java/com/dota/gg/user/service/UserServiceTest.java
@@ -1,32 +1,58 @@
 package com.dota.gg.user.service;
 
-import static org.junit.Assert.*;
 
+import java.util.List;
+import java.util.Date;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.dota.gg.user.entity.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:spring-context.xml"})
 public class UserServiceTest {
 
+	@Autowired
+	UserService userService;
+	
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		User user = new User();
+		user.setName("小小");
+		user.setPassword("tiny");
+		user.setAge(10);
+		user.setBirthday(new Date());
+		user.setEmail("1@1.com");
+		user.setPhone("13333333333");
+		Assert.assertEquals(userService.add(user), 1);
 	}
 
 	@Test
 	public void testUpdate() {
-		fail("Not yet implemented");
+		User user = new User();
+		user.setId(1L);
+		user.setName("山岭");
+		user.setPassword("tiny");
+		Assert.assertEquals(userService.update(user), 1);
 	}
 
 	@Test
 	public void testDelete() {
-		fail("Not yet implemented");
+		List<User> users = userService.getAll();
+		if(users.size()>0){
+			Assert.assertEquals(
+					userService.delete(users.get(users.size()-1).getId()), 1);
+		}
 	}
 
 	@Test
 	public void testGetAll() {
-		fail("Not yet implemented");
+		userService.getAll();
 	}
 
 }
